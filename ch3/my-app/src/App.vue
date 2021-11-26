19835a02aa7e6434e13ba4e656053112693608bf
Ramlala Yadav <easyexam01@gmail.com>
2021-11-27 01:41:23 +0530

    added ch3 by using cli

---
+++ ch3/my-app/src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" name="" id="" ref="name" />
+    <button @click="handleClick">click me</button>
+    <Modal />
+  </div>
+</template>
+<script >
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "My first App :)",
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.focus(); // by refering the elments of html you can get that element
+      // console.log(this.$refs.name.value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ ch3/my-app/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0px;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+</style>
